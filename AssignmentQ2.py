93a523f2650b061d36f05293f7ec822750bdc6a2
FatherofElias <supra_3_8@hotmail.com>
2024-06-23 11:49:40 -0500

    Q2T3 update

--- AssignmentQ2.py
+++ AssignmentQ2.py
@@ -24,3 +24,25 @@ projector = "premium projector and large screen" if attendees > 100 else "cheap 
 print("This is the audio/video setup we will use.")
 print(audio_system)
 print(projector)
+
+
+#Task 3 
+#Ask the user if they want vegetarian food. Reccomend veggie delight caterers if ys other wise gourmet meals caterers
+
+attendees = int(input("Enter number of attendees: "))
+venue = "large hall" if attendees > 100 else "conference room"
+print(venue)
+audio_system = "Stage speaker setup" if attendees > 100 else "small speaker setup"
+projector = "premium projector and large screen" if attendees > 100 else "cheap projector with the normal screen size"
+print("This is the audio/video setup we will use.")
+print(audio_system)
+print(projector)
+
+#solution i just used an input string for if they wanted veg or non veg and a print statement for if and else
+
+
+catering_choice = input("Would you like Vegetarian? veg/non-veg: ")
+if catering_choice == "veg":
+    print("Let's go with Veggie Delight Caterers then!")
+else:
+    print("We'll go with Gourmet Meals Caterers then!")
